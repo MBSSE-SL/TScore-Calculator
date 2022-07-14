0e56075674563480e0fb1df00c905afa69d2d51e
Mohamed James <mohamedjames21@gmail.com>
2022-07-14 04:28:32 +0000

    parseint

--- app.js
+++ app.js
@@ -31,16 +31,15 @@ var tscores = new Vue({
     methods: {
       
         tscore_calculate(){
-           this.tqa =  50+10*(this.qa-this.avgqa)/this.stdqa
-           this.tva =  50+10*(this.va-this.avgva)/this.stdva
-           this.tmat =  50+10*(this.mat-this.avgmat)/this.stdmat
-           this.teng =  50+10*(this.eng-this.avgeng)/this.stdeng
-           this.tgs =  50+10*(this.gs-this.avggs)/this.stdgs
-           this.agg =  this.qa+this.va+this.mat+this.eng+this.gs
-           this.avgagg =  this.avgqa+this.avgva+this.avgmat+this.avgeng+this.avggs
-           this.tagg =  this.tqa+this.tva+this.tmat+this.teng+this.tgs
-           
-
+           this.tqa =  50+10*(parseInt(this.qa)-parseInt(this.avgqa))/this.stdqa
+           this.tva =  50+10*(parseInt(this.va)-parseInt(this.avgva))/this.stdva
+           this.tmat =  50+10*(parseInt(this.mat)-parseInt(this.avgmat))/this.stdmat
+           this.teng =  50+10*(parseInt(this.eng)-parseInt(this.avgeng))/this.stdeng
+           this.tgs =  50+10*(parseInt(this.gs)-parseInt(this.avggs))/this.stdgs
+           this.agg = 
+           this.agg =  parseInt(this.qa)+parseInt(this.va)+parseInt(this.mat)+parseInt(this.eng)+parseInt(this.gs)
+           this.avgagg =  parseInt(this.avgqa)+parseInt(this.avgva)+parseInt(this.avgmat)+parseInt(this.avgeng)+parseInt(this.avggs)
+           this.tagg =  parseInt(this.tqa)+parseInt(this.tva)+parseInt(this.tmat)+parseInt(this.teng)+parseInt(this.tgs)
     },
     
 }
